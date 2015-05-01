3ac141536f61760cd475c7b23d564e0eeaabb902
masa <masa@cyber.t.u-tokyo.ac.jp>
2015-05-02 03:02:50 +0900

    p39 countExchange ver haskell

---
+++ haskell/01/p39.hs
@@ -0,0 +1,12 @@
+-- p39 両替方法の数を数える
+-- 両替金額と利用可能硬化を与え，両替パターン数を返す
+countExchangePatterns :: Int -> [Int] -> Int
+countExchangePatterns _ []  = 0
+countExchangePatterns n (x:xs)
+    | n == 0    = 1
+    | n < 0     = 0
+    | otherwise = countExchangePatterns n xs + countExchangePatterns (n-x) (x:xs)
+
+main = do
+    print $ countExchangePatterns 10 [5, 1]
+    print $ countExchangePatterns 100 [50, 25, 10, 5, 1]
