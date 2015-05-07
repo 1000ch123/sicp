22673acec3a9408e84266ee644a2cd9acfe0fff2
masa <masa@cyber.t.u-tokyo.ac.jp>
2015-05-07 22:30:39 +0900

    solve ex1-33b ver haskell

--- haskell/01/ex1-33.hs
+++ haskell/01/ex1-33.hs
@@ -8,7 +8,13 @@ filteredAccumulate combiner nval fn nx fi a b
     | fi a      = combiner (fn a) (filteredAccumulate combiner nval fn nx fi (nx a) b)
     | otherwise = filteredAccumulate combiner nval fn nx fi (nx a) b
 
+filteredSum :: (Num a, Ord a) => (a->a) -> (a->a) -> (a->Bool) -> a -> a -> a
+filteredSum = filteredAccumulate (+) 0
 
+filteredProd :: (Num a, Ord a) => (a->a) -> (a->a) -> (a->Bool) -> a -> a -> a
+filteredProd = filteredAccumulate (*) 1
+
+-- 素数判定
 isPrime :: (Integral a) => a -> Bool
 isPrime a = fermatTest a [1..a-1]
 
@@ -32,13 +38,23 @@ mrsq n x
     | otherwise  = x2
     where x2 = mod (x*x) n
 
-
-filteredSum :: (Num a, Ord a) => (a->a) -> (a->a) -> (a->Bool) -> a -> a -> a
-filteredSum = filteredAccumulate (+) 0
-
+-- 素数二乗和
 sumPrimeSquare :: (Num a, Ord a, Integral a) => a -> a -> a
 sumPrimeSquare = filteredSum (^2) (+1) isPrime
+
+-- GCD求める関数
+gcd' :: (Integral a) => a -> a -> a
+gcd' a b
+    | b == 0    = a
+    | otherwise = gcd' b (mod a b)
+
+isNPrime :: (Integral a) => a -> a -> Bool
+isNPrime a b = (gcd' a b == 1)
+
+prodNPrime :: (Num a, Ord a, Integral a) => a -> a
+prodNPrime a = filteredProd (id) (+1) (isNPrime a) 1 a
 
 
 main = do
     print $ sumPrimeSquare 2 5
+    print $ prodNPrime 7
