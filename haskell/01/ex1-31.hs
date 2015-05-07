941c5a14e78f3cb22d747fbaf690bd378e9d55a2
masa <masa@cyber.t.u-tokyo.ac.jp>
2015-05-07 20:32:12 +0900

    solve ex1-31b ver haskell

--- haskell/01/ex1-31.hs
+++ haskell/01/ex1-31.hs
@@ -1,10 +1,20 @@
 -- ex1-31
 
-
+-- 再帰プロセス
 product' :: (Num a, Ord a) => (a->a) -> (a->a) -> a -> a -> a
 product' fn nx a b
     | a > b     = 1
     | otherwise = fn a * product' fn nx (nx a) b
 
+-- 反復プロセス
+product'' :: (Num a, Ord a) => (a->a) -> (a->a) -> a -> a -> a
+product'' fn nx a b = iter a 1
+    where
+        iter a res
+            | a > b     = res
+            | otherwise = iter (nx a) (fn a * res)
+
 main = do
-    print $ product' (^2) (+1) 1 3
+    print $ product'  (^2) (+1) 1 3
+    print $ product'' (^2) (+1) 1 3
+
