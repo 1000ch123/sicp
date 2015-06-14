e631d5b364b65c253bfbe4cd0c157dcb4a2be4ad
masa <masa.guruto@gmail.com>
2015-06-15 01:00:21 +0900

    solve ex1-43 ver haskell

---
+++ haskell/01/ex1-43.hs
@@ -0,0 +1,15 @@
+-- ex1-43
+-- 指定関数をn回繰り返す関数
+
+type Fn = Int -> Int
+type Transform = Fn -> Fn
+
+repeated :: Int -> Transform
+repeated 1 fn = fn
+repeated n fn = fn . repeated (n-1) fn
+
+doubleSquare :: Fn
+doubleSquare = repeated 2 (^2)
+
+main = do
+    print $ doubleSquare 5
