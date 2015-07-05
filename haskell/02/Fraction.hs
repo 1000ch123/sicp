c901352cdedc5880c3cd711bc66b7ec076796b08
masa <masa.guruto@gmail.com>
2015-07-06 01:32:57 +0900

    impl.makeRatに約分機能追加

--- haskell/02/Fraction.hs
+++ haskell/02/Fraction.hs
@@ -5,28 +5,30 @@ module Fraction(
 
 -- |makeRat
 -- >>> makeRat 1 2
--- (1.0,2.0)
-makeRat :: Float -> Float -> (Float, Float)
-makeRat a b = (a,b)
-
+-- (1,2)
+-- >>> makeRat 2 4
+-- (1,2)
+makeRat :: Int -> Int -> (Int, Int)
+makeRat a b = (div a g, div b g)
+    where g = gcd a b
 -- |numer
 -- >>> let a = makeRat 2 3
 -- >>> numer a
--- 2.0
-numer :: (Float,Float) -> Float
+-- 2
+numer :: (Int,Int) -> Int
 numer = fst
 
 -- |denom
 -- >>> let a = makeRat 2 3
 -- >>> denom a
--- 3.0
-denom :: (Float, Float) -> Float
+-- 3
+denom :: (Int, Int) -> Int
 denom = snd
 
 -- |printRat
 -- >>> let a = makeRat 2 3
 -- >>> printRat a
--- "2.0/3.0"
+-- "2/3"
 printRat :: (Show a) => (a,a) -> IO()
 printRat (a,b) = print $ show a ++ "/" ++ show b
 
@@ -35,32 +37,32 @@ printRat (a,b) = print $ show a ++ "/" ++ show b
 -- >>> let a = makeRat 2 3
 -- >>> let b = makeRat 4 5
 -- >>> addRat a b
--- (22.0,15.0)
-addRat :: (Float, Float) -> (Float, Float) -> (Float, Float)
+-- (22,15)
+addRat :: (Int, Int) -> (Int, Int) -> (Int, Int)
 addRat a b = makeRat (numer a * denom b + numer b * denom a) (denom a * denom b)
 
 -- |subRat
 -- >>> let a = makeRat 2 3
 -- >>> let b = makeRat 4 5
 -- >>> subRat a b
--- (-2.0,15.0)
-subRat :: (Float, Float) -> (Float, Float) -> (Float, Float)
+-- (-2,15)
+subRat :: (Int, Int) -> (Int, Int) -> (Int, Int)
 subRat a b = makeRat (numer a * denom b - numer b * denom a) (denom a * denom b)
 
 -- |mulRat
 -- >>> let a = makeRat 2 3
 -- >>> let b = makeRat 4 5
 -- >>> mulRat a b
--- (8.0,15.0)
-mulRat :: (Float, Float) -> (Float, Float) -> (Float, Float)
+-- (8,15)
+mulRat :: (Int, Int) -> (Int, Int) -> (Int, Int)
 mulRat a b = makeRat (numer a * numer b) (denom a * denom b)
 
 -- |divRat
 -- >>> let a = makeRat 2 3
 -- >>> let b = makeRat 4 5
 -- >>> divRat a b
--- (10.0,12.0)
-divRat :: (Float, Float) -> (Float, Float) -> (Float, Float)
+-- (5,6)
+divRat :: (Int, Int) -> (Int, Int) -> (Int, Int)
 divRat a b = makeRat (numer a * denom b) (denom a * numer b)
 
 -- |sameRat
@@ -72,7 +74,7 @@ divRat a b = makeRat (numer a * denom b) (denom a * numer b)
 -- >>> let c = makeRat 2 3
 -- >>> sameRat a c
 -- True
-sameRat :: (Float, Float) -> (Float, Float) -> Bool
+sameRat :: (Int, Int) -> (Int, Int) -> Bool
 sameRat a b = numer a == numer b && denom a == denom b
 
 
