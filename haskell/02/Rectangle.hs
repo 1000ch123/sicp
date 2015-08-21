cd82294368c1e4dd39a4588c47688097f50ad911
masa <masa.guruto@gmail.com>
2015-08-21 21:23:15 +0900

    fix Rectangle.hs : Pointに関する処理をPointモジュールに委譲

--- haskell/02/Rectangle.hs
+++ haskell/02/Rectangle.hs
@@ -4,8 +4,8 @@ module Rectangle(
     calcPeriometer
 )where
 
+import Point
 -- 独自データ型で書きたいけど，プリミティブだけで頑張るぞい
-
 
 -- |makeRectangle
 -- >>> makeRectangle (1,2) (3,4)
@@ -17,15 +17,23 @@ makeRectangle a b = (a,b)
 -- >>> calcSquare ((1.0,5.0),(4.0,1.0))
 -- 12.0
 calcSquare :: ((Float,Float),(Float,Float)) -> Float
-calcSquare ((x1,y1),(x2,y2)) = dx * dy
-    where dx = abs(x1-x2)
+calcSquare rect = dx * dy
+    where x1 = xPoint . fst $ rect
+          y1 = yPoint . fst $ rect
+          x2 = xPoint . snd $ rect
+          y2 = yPoint . snd $ rect
+          dx = abs(x1-x2)
           dy = abs(y1-y2)
 
 -- | calcSquare
 -- >>> calcPeriometer ((1.0,5.0),(4.0,1.0))
 -- 14.0
 calcPeriometer :: ((Float,Float),(Float,Float)) -> Float
-calcPeriometer ((x1,y1),(x2,y2)) = 2 * (dx + dy)
-    where dx = abs(x1-x2)
+calcPeriometer rect = 2 * (dx + dy)
+    where x1 = xPoint . fst $ rect
+          y1 = yPoint . fst $ rect
+          x2 = xPoint . snd $ rect
+          y2 = yPoint . snd $ rect
+          dx = abs(x1-x2)
           dy = abs(y1-y2)
 
