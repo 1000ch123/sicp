6b7140098d4d1f84cf7381b392fa593ee46823e9
masa <masa.guruto@gmail.com>
2015-08-22 21:38:27 +0900

    refactor 範囲四則演算を高階関数化

--- haskell/02/Interval.hs
+++ haskell/02/Interval.hs
@@ -29,39 +29,56 @@ upperBound :: Interval -> Float
 upperBound (a,b) = max a b
 
 -- 四則演算
+calcInterval :: (Float -> Float -> Float) -> Interval -> Interval -> Interval
+calcInterval op i1 i2 =  makeInterval (minimum candidates) (maximum candidates)
+    where p1 = op (lowerBound i1) (lowerBound i2)
+          p2 = op (lowerBound i1) (upperBound i2)
+          p3 = op (upperBound i1) (lowerBound i2)
+          p4 = op (upperBound i1) (upperBound i2)
+          candidates = [p1,p2,p3,p4]
+
 -- | addInterval
 -- >>> addInterval (8,12) (12,18)
 -- (20.0,30.0)
 addInterval :: Interval -> Interval -> Interval
-addInterval i1 i2 = makeInterval (lowerBound i1 + lowerBound i2) (upperBound i1 + upperBound i2)
-
+addInterval = calcInterval (+)
 
 -- | subInterval
 -- >>> subInterval (8,12) (12,18)
 -- (-10.0,0.0)
 subInterval :: Interval -> Interval -> Interval
-subInterval i1 i2 = makeInterval (minimum candidates) (maximum candidates)
-    where p1 = lowerBound i1 - lowerBound i2
-          p2 = lowerBound i1 - upperBound i2
-          p3 = upperBound i1 - lowerBound i2
-          p4 = upperBound i1 - upperBound i2
-          candidates = [p1,p2,p3,p4]
+subInterval = calcInterval (-)
 
 -- | mulInterval
 -- >>> mulInterval (8,12) (12,18)
 -- (96.0,216.0)
 mulInterval :: Interval -> Interval -> Interval
-mulInterval i1 i2 = makeInterval (minimum candidates) (maximum candidates)
-    where p1 = lowerBound i1 * lowerBound i2
-          p2 = lowerBound i1 * upperBound i2
-          p3 = upperBound i1 * lowerBound i2
-          p4 = upperBound i1 * upperBound i2
-          candidates = [p1,p2,p3,p4]
+mulInterval = calcInterval (*)
 
 -- | divInterval
 -- >>> divInterval (8,12) (2,4)
 -- (2.0,6.0)
 divInterval :: Interval -> Interval -> Interval
-divInterval i1 i2 = mulInterval i1 i2'
-    where i2' = makeInterval (1 / upperBound i2) (1 / lowerBound i2)
+divInterval = calcInterval (/)
+
+
+-- 頑張って実装するversion
+{-addInterval i1 i2 = makeInterval (lowerBound i1 + lowerBound i2) (upperBound i1 + upperBound i2)-}
+
+{-subInterval i1 i2 = makeInterval (minimum candidates) (maximum candidates)-}
+    {-where p1 = lowerBound i1 - lowerBound i2-}
+          {-p2 = lowerBound i1 - upperBound i2-}
+          {-p3 = upperBound i1 - lowerBound i2-}
+          {-p4 = upperBound i1 - upperBound i2-}
+          {-candidates = [p1,p2,p3,p4]-}
+
+{-mulInterval i1 i2 = makeInterval (minimum candidates) (maximum candidates)-}
+    {-where p1 = lowerBound i1 * lowerBound i2-}
+          {-p2 = lowerBound i1 * upperBound i2-}
+          {-p3 = upperBound i1 * lowerBound i2-}
+          {-p4 = upperBound i1 * upperBound i2-}
+          {-candidates = [p1,p2,p3,p4]-}
+
+{-divInterval i1 i2 = mulInterval i1 i2'-}
+    {-where i2' = makeInterval (1 / upperBound i2) (1 / lowerBound i2)-}
 
