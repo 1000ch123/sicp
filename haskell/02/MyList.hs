ccc029c201f8d7e42baf8d057b673b54e2de13ed
masa <masa.guruto@gmail.com>
2015-08-24 22:51:44 +0900

    impl. ref,length,append ver haskell

--- haskell/02/MyList.hs
+++ haskell/02/MyList.hs
@@ -1,6 +1,35 @@
 module MyList(
     myLast,
+    myReverse
 )where
+
+-- リストへの基本命令(cons/car/cdr)のみで，
+-- リストへの各種操作を実装するぜ
+-- Haskellで言うと (:),head,tail
+-- case文のかわりにパターンマッチはokとする
+
+-- | myListRef
+-- >>> myListRef [0,1,2,3,4,5] 0
+-- 0
+-- >>> myListRef [0,1,2,3,4,5] 2
+-- 2
+myListRef :: [a] -> Int -> a
+myListRef xs 0 = head xs
+myListRef xs n = myListRef (tail xs) (n-1)
+
+-- | myListRef
+-- >>> myLength [0,1,2,3,4,5]
+-- 6
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+-- | myAppend
+-- >>> myAppend [1,2] [5,6]
+-- [1,2,5,6]
+myAppend :: [a] -> [a] -> [a]
+myAppend [] ys     = ys
+myAppend (x:xs) ys = x:myAppend xs ys
 
 -- | myLast
 -- >>> myLast [1,2,3,4]
@@ -10,3 +39,5 @@ myLast [x]    = x
 myLast (x:xs) = myLast xs
 
 
+myReverse :: [a] -> [a]
+myReverse (x:xs) = xs
