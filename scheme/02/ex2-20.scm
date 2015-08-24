6d9e64376c14fd14fdaeec745d893c2bc80c75e3
masa <masa.guruto@gmail.com>
2015-08-24 23:55:55 +0900

    solve ex2-20
    - haskellでは可変引数対応が面倒感あったのでschemeで解いた

---
+++ scheme/02/ex2-20.scm
@@ -0,0 +1,17 @@
+(use slib)
+(require 'trace)
+
+(define (same-parity x0 . xs)
+  (cons x0 (step x0 xs)))
+
+(define (same a b)
+  (= (remainder (- a b) 2) 0))
+
+; stepに対しては . xs が「ひとつのリスト」として渡されるっぽい
+(define (step x0 xs)
+  (cond ((null? xs) (list))
+        ((same x0 (car xs)) (cons (car xs) (step x0 (cdr xs))))
+        (else (step x0 (cdr xs)))
+        ))
+
+(print (same-parity 1 2 3 4 5 6 7 8 9))
