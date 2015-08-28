7a35df221e49352ccd536e6203e3c32edafcbedc
vg-1000ch <Masahiko_Izumi@voyagegroup.com>
2015-08-28 21:54:05 +0900

    solve ex2-30

--- scheme/02/ex2-30.scm
+++ scheme/02/ex2-30.scm
@@ -4,4 +4,11 @@
 (include "util")
 
 
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(print (square-tree (list 1 2 (list 3 4) (list 5))))
 
