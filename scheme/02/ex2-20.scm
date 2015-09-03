efda6ebc97c2f4093ce7885afceba68c8b5c573d
vg-1000ch <Masahiko_Izumi@voyagegroup.com>
2015-09-03 21:24:40 +0900

    add comment ex2-20

--- scheme/02/ex2-20.scm
+++ scheme/02/ex2-20.scm
@@ -4,12 +4,13 @@
 (define (same-parity x0 . xs)
   (cons x0 (step x0 xs)))
 
+; 差を２で割ったあまりでパリティチェック
 (define (same a b)
   (= (remainder (- a b) 2) 0))
 
 ; stepに対しては . xs が「ひとつのリスト」として渡されるっぽい
 (define (step x0 xs)
-  (cond ((null? xs) (list))
+  (cond ((null? xs) '())
         ((same x0 (car xs)) (cons (car xs) (step x0 (cdr xs))))
         (else (step x0 (cdr xs)))
         ))
